ee8092b1757c380d69a7d5d246204df9833f24ca
Vogelsang, Stefan <stefan.vogelsang@ise.de>
2016-01-13 09:03:49 +0100

    Bump version to 1.0.0

--- src/NUnit.ManualTest/Properties/AssemblyInfo.cs
+++ src/NUnit.ManualTest/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("NUnit.ManualTest")]
 [assembly: AssemblyProductAttribute("NUnit.ManualTest")]
 [assembly: AssemblyDescriptionAttribute("NUnit.ManualTest is designed to include manual tests in the NUnit Framework infrastructure.")]
-[assembly: AssemblyVersionAttribute("0.1.1")]
-[assembly: AssemblyFileVersionAttribute("0.1.1")]
+[assembly: AssemblyVersionAttribute("1.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.1";
+        internal const string Version = "1.0.0";
     }
 }
